4f63b22a712d0e5cefe9e324ac6ceb57010f8810
Devon Poon Woo <devon.poonwoo@gmail.com>
2021-10-20 18:15:40 -0400

    Have background scrolling.

---
+++ Assets/Scripts/bgScroll.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgScroll : MonoBehaviour
+{
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x <= -19.18) {
+            transform.position = new Vector3(0, 0, 0);
+        }
+    }
+}
